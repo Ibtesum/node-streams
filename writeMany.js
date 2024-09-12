85e82f608ce873722ae3a469640720a10ba9a990
Ibtesum <aninda7152@gmail.com>
2024-09-13 03:50:17 +0600

    using streams implement same 1 lakh writes in a file

--- writeMany.js
+++ writeMany.js
@@ -14,18 +14,34 @@
 //   console.timeEnd("TotalTime");
 // })();
 
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
 // Execution time : around 1 seconds
 // CPU usage: 100% (one core)
 // Memory usage: 18MB
-(() => {
-  console.time("writeMany");
-  fs.open("test.txt", "w", (err, fd) => {
-    for (let i = 1; i <= 100000; i++) {
-      const buff = Buffer.from(` ${i} `, "utf-8");
-      fs.writeSync(fd, buff);
-    }
-  });
-  console.timeEnd("writeMany");
+// (() => {
+//   console.time("writeMany");
+//   fs.open("test.txt", "w", (err, fd) => {
+//     for (let i = 1; i <= 100000; i++) {
+//       const buff = Buffer.from(` ${i} `, "utf-8");
+//       fs.writeSync(fd, buff);
+//     }
+//   });
+//   console.timeEnd("writeMany");
+// })();
+
+const fs = require("node:fs/promises");
+// Execution time :
+// CPU usage: 100% (one core)
+// Memory usage:
+(async () => {
+  console.time("TotalTime");
+  const fileHandler = await fs.open("test.txt", "w");
+  const stream = fileHandler.createWriteStream();
+  for (let i = 1; i <= 100000; i++) {
+    const buff = Buffer.from(` ${i} `, "utf-8");
+    stream.write(buff);
+  }
+  await fileHandler.close();
+  console.timeEnd("TotalTime");
 })();
