72466e6a28e3956c4f3a07e382bb6daaa37d7b9d
Ibtesum <aninda7152@gmail.com>
2024-09-22 07:21:33 +0600

    Resolve splitting issue & Implementing only even number write functionality

--- read-big/readBig.js
+++ read-big/readBig.js
@@ -1,6 +1,7 @@
 const fs = require("node:fs/promises");
 
 (async () => {
+  console.time("ReadWriteTime");
   const fileHandleRead = await fs.open("src.txt", "r");
   const fileHandleWrite = await fs.open("dest.txt", "w");
 
@@ -9,15 +10,42 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
+  let split = "";
+
   streamRead.on("data", (chunk) => {
-    // MUST DO: IF you dont want to crash your system
-    if (!streamWrite.write(chunk)) {
-      streamRead.pause();
+    // TODO: Selectively writing our data from the readable stream
+
+    const numbers = chunk.toString("utf-8").split("  ");
+
+    if (Number(numbers[0]) !== Number(numbers[1]) - 1) {
+      if (split) numbers[0] = split.trim() + numbers[0].trim();
     }
+
+    if (
+      Number(numbers[numbers.length - 2]) + 1 !==
+      Number(numbers[numbers.length - 1])
+    ) {
+      split = numbers.pop();
+    }
+
+    numbers.forEach((number) => {
+      let n = Number(number);
+      if (n % 2 === 0) {
+        if (!streamWrite.write(" " + n + " ")) {
+          // MUST DO: IF you dont want to crash your system
+          streamRead.pause();
+        }
+      }
+    });
   });
 
   // MUST DO: IF you dont want to crash your system
   streamWrite.on("drain", () => {
     streamRead.resume();
+  });
+
+  streamRead.on("end", () => {
+    console.log("Done Reading");
+    console.timeEnd("ReadWriteTime");
   });
 })();
