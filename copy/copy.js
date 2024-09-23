92264cc9350a3895a95700d314177cd1a01e3e1c
Ibtesum <aninda7152@gmail.com>
2024-09-23 07:58:13 +0600

    Own implementation of stream using Buffer

---
+++ copy/copy.js
@@ -0,0 +1,63 @@
+const fs = require("node:fs/promises");
+
+// MEMORY Usage: Very high(depending on the file size);
+// Execution time: Relatively higher than stream implementation.
+// (async () => {
+//   console.time("copy");
+//   const destFile = await fs.open("text-copy.txt", "w");
+//   const result = await fs.readFile("test.txt");
+
+//   await destFile.write(result);
+//   console.timeEnd("copy");
+// })();
+
+
+// Our own implementation of stream using buffer.
+(async () => {
+  console.time("copy");
+
+  const srcFile = await fs.open("text-small.txt", "r");
+  const destFile = await fs.open("text-copy.txt", "w");
+
+  let bytesRead = -1;
+  while (bytesRead !== 0) {
+    const readResult = await srcFile.read();
+    bytesRead = readResult.bytesRead;
+
+    // console.log(readResult.bytesRead);
+
+    if (bytesRead !== 16384) {
+      const indexOfNotFilled = readResult.buffer.indexOf(0);
+      const newBuffer = Buffer.alloc(indexOfNotFilled);
+      readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+      destFile.write(newBuffer);
+    } else {
+      destFile.write(readResult.buffer);
+    }
+
+    // TODO: This code below needs to be fixed. 
+    // if (readResult.bytesRead < 16384 && readResult.bytesRead !== 0) {
+    //   const slicedBuffer = readResult.buffer.subarray(
+    //     0,
+    //     readResult.buffer.length
+    //   );
+    //   console.log("slicedBuffer", slicedBuffer[14360]);
+    //   await destFile.write(slicedBuffer);
+    // } else if (readResult.bytesRead !== 0) {
+    //   await destFile.write(readResult.buffer);
+    // }
+  }
+  console.log("Hello".red);
+  console.timeEnd("copy");
+})();
+
+
+
+(async () => {
+  console.time("copy");
+
+  const srcFile = await fs.open("text-small.txt", "r");
+  const destFile = await fs.open("text-copy.txt", "w");
+
+  
+})()
